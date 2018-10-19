5556557b14e68925b2ea14fc56e6ccd13614b8b7
goeroeku <goeroeku@gmail.com>
2018-10-19 15:48:25 +0700

    restore project node-restify-prosgres

---
+++ node-restify-postgres-new/src/config/settings.js
@@ -0,0 +1,12 @@
+export default {
+  name: "node-restify-postgres",
+  version: "1.0.0",
+  port: 3000,
+  db: {
+    host: "localhost",
+    port: 5432,
+    database: "tefa-pgsql-clone",
+    user: "postgres",
+    password: "postgres"
+  }
+};
